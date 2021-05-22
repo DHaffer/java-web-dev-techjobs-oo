824d02c0a0d0c7aa34b0651960c5c95d361be801
Daryn <dhaffer1991@gmail.com>
2021-05-22 12:53:20 -0500

    Refactored

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,16 +1,18 @@
 package org.launchcode.techjobs_oo.Tests;
 
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 import org.launchcode.techjobs_oo.*;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class JobTest {
 
     @Test
-    public void testSettingJobId() {
+    public void a_testSettingJobId() {
         Job test_job1 = new Job("Junior Developer", new Employer("Boeing"), new Location("St. Louis"), new PositionType("Backend"), new CoreCompetency("Patience"));
         Job test_job2 = new Job("Senior Developer", new Employer("Google"), new Location("Silicon Valley"), new PositionType("Frontend"), new CoreCompetency("Team Player"));
         assertTrue(test_job1.getId() != test_job2.getId());
@@ -19,10 +21,10 @@ public class JobTest {
     }
 
     @Test
-    public void testJobConstructorSetsAllFields() {
+    public void b_testJobConstructorSetsAllFields() {
         Job test_job3 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         System.out.println(test_job3.toString());
-        assertEquals(5, test_job3.getId());
+        assertEquals(3, test_job3.getId());
 
         assertTrue(test_job3.getName() instanceof String);
         assertEquals("Product Tester", test_job3.getName());
@@ -41,7 +43,7 @@ public class JobTest {
     }
 
     @Test
-    public void testJobsForEquality() {
+    public void c_testJobsForEquality() {
         Job test_job4 = new Job("Junior Developer", new Employer("Facebook"), new Location("Silicon Valley"), new PositionType("Front End"), new CoreCompetency("Spunk"));
         Job test_job5 = new Job("Junior Developer", new Employer("Facebook"), new Location("Silicon Valley"), new PositionType("Front End"), new CoreCompetency("Spunk"));
         assertFalse(test_job4.equals(test_job5));
@@ -50,11 +52,11 @@ public class JobTest {
     }
 
     @Test
-    public void toStringCreatesBlankFirstAndLastLine() {
+    public void d_toStringCreatesBlankFirstAndLastLine() {
         Job test_job6 = new Job("Senior Developer", new Employer("Verizon"), new Location("St. Louis"), new PositionType("Back End"), new CoreCompetency("Determination"));
         System.out.println(test_job6.toString());
         assertEquals("\n" +
-                "ID: " + 3 +
+                "ID: " + 6 +
                 "\n" +
                 "Name: " + "Senior Developer" +
                 "\n" +
@@ -70,11 +72,11 @@ public class JobTest {
     }
 
     @Test
-    public void toStringContainsLabelForEachField() {
+    public void e_toStringContainsLabelForEachField() {
         Job test_job7 = new Job("Senior Developer", new Employer("Verizon"), new Location("St. Louis"), new PositionType("Back End"), new CoreCompetency("Determination"));
         System.out.println(test_job7.toString());
         assertEquals("\n" +
-                "ID: " + 6 +
+                "ID: " + 7 +
                 "\n" +
                 "Name: " + "Senior Developer" +
                 "\n" +
@@ -89,11 +91,11 @@ public class JobTest {
     }
 
     @Test
-    public void toStringContainsNotAvailableIfEmpty() {
+    public void f_toStringContainsNotAvailableIfEmpty() {
         Job test_job8 = new Job("Senior Developer", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
         System.out.println(test_job8.toString());
         assertEquals("\n" +
-                "ID: " + 9 +
+                "ID: " + 8 +
                 "\n" +
                 "Name: " + "Senior Developer" +
                 "\n" +
@@ -108,7 +110,7 @@ public class JobTest {
     }
 
     @Test
-    public void toStringContainsOopsIfOnlyIdField() {
+    public void g_toStringContainsOopsIfOnlyIdField() {
         Job test_job9 = new Job("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
         System.out.println(test_job9.toString());
         assertEquals("OOPS! This job does not seem to exist.", test_job9.toString());
